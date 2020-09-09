b9e867dc42908f091b3aa4846c9a9b35f8110d9e
Kim-SiHwan <tjdans4166@naver.com>
2020-09-09 16:37:00 +0900

    Paging

--- src/main/java/jpaboard/jpaboard/RequestDto/PageRequestDto.java
+++ src/main/java/jpaboard/jpaboard/RequestDto/PageRequestDto.java
@@ -2,6 +2,9 @@ package jpaboard.jpaboard.RequestDto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @Getter
 public class PageRequestDto {
@@ -10,6 +13,25 @@ public class PageRequestDto {
 
     private int page;
     private int size;
+
+    public PageRequestDto(){
+        page=1;
+        size=default_size;
+    }
+
+    public void setPage ( int page ){
+        this.page = page<=0 ? 1 : page;
+    }
+
+    public void setSize( int size ){
+        this.size = size<default_size || size>default_max_size ? default_size : size;
+    }
+
+    public Pageable makePage(int direction , String key){
+        Sort.Direction dir = direction==0 ? Sort.Direction.DESC : Sort.Direction.ASC;
+        return PageRequest.of(page-1,size,dir,key);
+    }
+
 
 
 }
