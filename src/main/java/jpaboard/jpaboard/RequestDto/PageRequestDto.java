411d37e5be98429ca7b2af9aad73b0703f97fedf
Kim-SiHwan <tjdans4166@naver.com>
2020-09-09 16:31:22 +0900

    Paging

--- src/main/java/jpaboard/jpaboard/RequestDto/PageRequestDto.java
+++ src/main/java/jpaboard/jpaboard/RequestDto/PageRequestDto.java
@@ -5,6 +5,9 @@ import lombok.Setter;
 
 @Getter
 public class PageRequestDto {
+    private static final int default_size = 10;
+    private static final int default_max_size = 50;
+
     private int page;
     private int size;
 
