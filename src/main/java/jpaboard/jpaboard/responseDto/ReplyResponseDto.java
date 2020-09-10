56e26f32cd409269d050075be1bc1221c87cfced
Kim-SiHwan <tjdans4166@naver.com>
2020-09-11 00:24:41 +0900

    ReplyResponseDto

---
+++ src/main/java/jpaboard/jpaboard/responseDto/ReplyResponseDto.java
@@ -0,0 +1,23 @@
+package jpaboard.jpaboard.responseDto;
+
+import jpaboard.jpaboard.domain.Reply;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ReplyResponseDto {
+    private Long id;
+    private String userName;
+    private String content;
+    private int likes;
+    private LocalDateTime createDate;
+
+    public ReplyResponseDto(Reply reply){
+        this.id= reply.getId();
+        this.userName=reply.getMember().getUserName();
+        this.content= reply.getContent();
+        this.likes= reply.getLikes();
+        this.createDate=getCreateDate();
+    }
+}
