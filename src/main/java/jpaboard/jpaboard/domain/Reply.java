357a095b835c63856a9b7892f397b9f2bc13f599
Kim-SiHwan <tjdans4166@naver.com>
2020-09-10 20:02:21 +0900

    댓글Entity

--- src/main/java/jpaboard/jpaboard/domain/Reply.java
+++ src/main/java/jpaboard/jpaboard/domain/Reply.java
@@ -1,17 +1,16 @@
 package jpaboard.jpaboard.domain;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Builder
 @ToString(exclude = {"member","board"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Reply {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,17 +19,29 @@ public class Reply {
 
     private String content;
 
-    private LocalDateTime replyDate;
+    private LocalDateTime createDate;
 
     private int likes;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
+
+    @Builder(builderClassName = "createReply", builderMethodName = "createReply")
+    public Reply(Long id, String content, LocalDateTime createDate, int likes, Member member, Board board) {
+        this.id = id;
+        this.content = content;
+        this.createDate = createDate;
+        this.likes = likes;
+        this.member = member;
+        this.board = board;
+    }
 
     public void changeLikes(int likes){
         this.likes+=likes;
@@ -39,6 +50,11 @@ public class Reply {
     public void setMember(Member member){
         this.member=member;
         this.member.getReplies().add(this);
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+        this.board.getReplies().add(this);
     }
 
 
