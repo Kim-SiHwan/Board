f6c572d8cc57b3f780d95d308e081be2999070eb
Kim-SiHwan <tjdans4166@naver.com>
2020-09-11 22:06:37 +0900

    addSecurity

---
+++ src/main/java/jpaboard/jpaboard/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package jpaboard.jpaboard.config;
+
+import jpaboard.jpaboard.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/boards/home").permitAll()
+                .antMatchers("/boards/view").permitAll()
+                .antMatchers("/boards/uploadBoard").hasAnyRole("ADMIN","USER")
+                .antMatchers("/h2/*").permitAll()
+                .antMatchers("/replies/**").permitAll();
+        http.csrf()
+                .ignoringAntMatchers("/replies/**")
+                .ignoringAntMatchers("/h2/**")
+                .ignoringAntMatchers("/boards/uploadBoard");
+        http.headers().frameOptions().disable();
+        http.formLogin().defaultSuccessUrl("/boards/home");
+        http.logout().logoutSuccessUrl("/boards/home");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
